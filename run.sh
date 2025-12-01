49194fbf1162349eb9093caada9b2d8b06c7b280
jonasb <jonas.bonnaudet@mindroad.se>
2025-12-01 14:42:43 +0100

    parameter for install

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #/bin/bash
 
-printf "n\n\n\n+1M\nef02\nn\n\n\n\n\nw\ny\n" | gdisk /dev/sda
+device=${1:-"sda"}
 
-mkfs.ext4 /dev/sda2 && mount dev/sda2 /mnt
+printf "n\n\n\n+1M\nef02\nn\n\n\n\n\nw\ny\n" | gdisk "/dev/${device}"
+
+mkfs.ext4 "/dev/${device}2" && mount "/dev/${device}2" /mnt
 
 sed -i 's/^ParallelDownloads = 5/ParallelDownloads = 100/' /etc/pacman.conf
 
 pacstrap /mnt base linux linux-firmware grub
 
-genfstab -U /mnt > /mnt/etc/fstab; arch-chroot /mnt bash -c 'grub-install --target=i386-pc /dev/sda; grub-mkconfig -o /boot/grub/grub.cfg'
+genfstab -U /mnt > /mnt/etc/fstab; arch-chroot /mnt bash -c "grub-install --target=i386-pc /dev/${device}; grub-mkconfig -o /boot/grub/grub.cfg"
